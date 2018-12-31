b9d1f270b3d9e987fceb61ff82befa069a685dfd
ken <dragoonqp@aliyun.com>
2018-12-31 20:28:00 +0800

    update

--- pool.js
+++ pool.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 var pool = mysql.createPool({
 	host:'w.rdc.sae.sina.com.cn',
 	port:3306,
-	user:'root',
-	password:'',
-	database:'aqiFruit',
+	user:'ml11yk110o',
+	password:'01x2j4h0ilih5yjw0w5h1240h3xii5mkmjwk5014',
+	database:'app_aqfruit',
 	connectionLimit:20,
 	multiplestatements:true
 });
